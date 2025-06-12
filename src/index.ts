a2f961eb3127757fa6c39345e86803040caa77f0
jurgob <jurgob@users.noreply.github.com>
2025-06-12 01:06:12 +0100

    Refactor PokeApi to use Core for HTTP requests and improve request logging

--- src/index.ts
+++ src/index.ts
@@ -13,10 +13,33 @@ function convertPaginationData<T extends PaginationData>(response : T): {next: b
 }
 
 
-function PokeApi(){
-    const fetcher = fetch
-    async function httpRquest<T extends z.ZodTypeAny>(request:Request, schema: T): Promise<z.infer<T>>{
-        const response = await fetcher(request);
+type SDKOptions = {
+    fetch?: typeof fetch;
+    baseUrl?: string;
+    headers?: Record<string, string>;
+}
+
+type SDKHTTPRequest = {
+    endpoint: string;
+}
+
+// https://pokeapi.co/
+function Core(coreOptions: SDKOptions = {
+    baseUrl: "https://pokeapi.co/api/v2",
+    fetch: globalThis.fetch
+}) {
+    async function httpRquest<T extends z.ZodTypeAny>(request:SDKHTTPRequest, schema: T): Promise<z.infer<T>>{
+        console.log("Making HTTP request to:", request.endpoint);
+        const url = `${coreOptions.baseUrl}${request.endpoint}`;
+        // const url = new URL(request.endpoint, CoreOptions.baseUrl);
+        console.log("Request URL:", url.toString());
+        const response = await fetch(new Request(url.toString(), {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                ...coreOptions.headers
+            }
+        }));
          if (!response.ok) {
             throw new Error(`Error during http request: ${response.statusText}`);
          }
@@ -24,11 +47,20 @@ function PokeApi(){
             return schema.parseAsync(raw);
      }
 
-    const getPokemon = async (name: string) => {
-        return httpRquest(new Request(`https://pokeapi.co/api/v2/pokemon/${name}?limit=1`), PockemonSchema);
-    }
-    const getPokemonList = async (limit: number = 20, offset: number = 0) => {
-        const resp = await httpRquest(new Request(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`), PockemonListSchema);
+     return {
+        httpRquest
+     }
+}
+
+type Core = ReturnType<typeof Core>;
+
+ const getPokemon = async (core: Core, name: string) => {
+    return core.httpRquest({endpoint: `/pokemon/${name}`}, PockemonSchema);
+}
+
+ const getPokemonList = async (core: Core, limit: number = 20, offset: number = 0) => {
+        const endpoint = `/pokemon?limit=${limit}&offset=${offset}`;
+        const resp = await core.httpRquest({ endpoint}, PockemonListSchema);
         const result = {
             ...convertPaginationData(resp),
             results:resp.results.map((item) => ({
@@ -38,11 +70,15 @@ function PokeApi(){
         }
         return result
     }
-    const getGeneration = async (name: string) => {
-            return httpRquest(new Request(`https://pokeapi.co/api/v2/generation/${name}`),GenerationSchema);
-        }   
-    const getGenerationList = async (limit: number = 10, offset: number = 0) => {
-        const resp = await  httpRquest(new Request(`https://pokeapi.co/api/v2/generation?limit=${limit}&offset=${offset}`), GenerationListSchema);
+
+const getGeneration = async (core: Core,name: string) => {
+        const endpoint = `/generation/${name}`;
+            return core.httpRquest({endpoint},GenerationSchema);
+        } 
+
+ const getGenerationList = async (core: Core,limit: number = 10, offset: number = 0) => {
+        const endpoint = `/generation?limit=${limit}&offset=${offset}`;
+        const resp = await  core.httpRquest({endpoint}, GenerationListSchema);
         const result = {
             ...convertPaginationData(resp),
             results:resp.results.map((item) => ({
@@ -52,18 +88,19 @@ function PokeApi(){
         return result;
         
     }
-    
 
+function PokeApi(options?: SDKOptions) {
+    const core = Core(options);
     return {
-       pockemon: {
-            get:  getPokemon,
-            list: getPokemonList
-       },
-       generation:{
-            get: getGeneration,
-            list: getGenerationList
-       } 
-    };    
+        pockemon: {
+             get:  getPokemon.bind(null, core),
+             list: getPokemonList.bind(null, core)
+        },
+        generation:{
+             get: getGeneration.bind(null, core),
+             list: getGenerationList.bind(null, core)
+        } 
+    };
 }
 
 
@@ -71,13 +108,13 @@ async function main() {
     console.log("PokeApi initialized");
     const pokeApi = PokeApi();
     console.log("Pockemon List:");
-    const pockemonList = await pokeApi.pockemon.list(10, 1);
+    // const pockemonList = await pokeApi.pockemon.list(10, 1);
 
-    console.dir(pockemonList, { depth: null });
+    // console.dir(pockemonList, { depth: null });
     
-    // const pockemon = await pokeApi.pockemon.get("pikachu")
-    // console.log("Pockemon:");
-    // console.dir(pockemon, { depth: null });
+    const pockemon = await pokeApi.pockemon.get("pikachu")
+    console.log("Pockemon:");
+    console.dir(pockemon, { depth: null });
     // const generation = await pokeApi.getGeneration("generation-i");
     // console.log("Generation:", generation);
     // console.log("PokeApi finished");
